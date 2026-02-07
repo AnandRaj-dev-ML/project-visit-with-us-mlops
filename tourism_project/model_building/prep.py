afd87b1041b8ae9b17842e012d33c52dd85d8fd9
AnandRaj <43278669+AnandRaj-dev-ML@users.noreply.github.com>
2026-02-07 18:14:51 +0530

    Update prep.py fixed error because this was already dropped

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -17,7 +17,7 @@ df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
 # Drop the unique identifier
-df.drop(columns=['Sl'], inplace=True)
+# df.drop(columns=['Sl'], inplace=True)
 
 # Drop the unique identifier
 df.drop(columns=['CustomerID'], inplace=True)
